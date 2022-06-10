a3988a7f7a281a9aa6293249fdb32d9f4e39b186
Mateus Santos <m.santos.azeve@gmail.com>
2022-06-10 03:47:10 -0300

    tenta adicionar um personagem ao firebase - FirebaseError

--- src/components/CardMarvel/index.jsx
+++ src/components/CardMarvel/index.jsx
@@ -1,14 +1,41 @@
 import React from "react";
 import * as Styled from "./styles";
 
+import { FirebaseConfig } from "../../services";
+import { addDoc, getFirestore, collection } from "firebase/firestore";
+
+const database = getFirestore(FirebaseConfig);
+const userCollectionRef = collection(database, "characters");
+
 export default (props) => {
+  async function saveFavoriteCharacter() {
+    try {
+      const character = await addDoc(userCollectionRef, {
+        id: props.id,
+        name: props.name,
+        description: props.description,
+        thumbnail: props.thumbnail,
+      });
+
+      console.log(character);
+    } catch (e) {
+      console.log("Error addDoc: ", e);
+    }
+  }
+
   return (
     <Styled.Wrapper>
       <Styled.Image variant="top" src={props.thumbnail} alt={props.name} />
       <Styled.Content>
         <Styled.Title>{props.name}</Styled.Title>
         <Styled.Description>{props.description}</Styled.Description>
-        <Styled.SaveButton>Go somewhere</Styled.SaveButton>
+        <Styled.SaveButton
+          onClick={() => {
+            saveFavoriteCharacter();
+          }}
+        >
+          Go somewhere
+        </Styled.SaveButton>
       </Styled.Content>
     </Styled.Wrapper>
   );
