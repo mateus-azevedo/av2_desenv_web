600202cf01a769e42eef81f3c876601d207d2a3d
Mateus Santos <m.santos.azeve@gmail.com>
2022-06-10 21:56:58 -0300

    renderiza lista se estiver logado

--- src/features/main/pages/home/index.jsx
+++ src/features/main/pages/home/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import * as Styled from "./styles";
 
 import {
@@ -8,26 +8,34 @@ import {
 } from "../../../../components";
 // import { WrapperPage } from "@components";
 
+import { AuthGoogle } from "../../../../context";
+
 export default (props) => {
+  const { signed, user } = useContext(AuthGoogle.Context);
+  const userName = JSON.parse(user).displayName;
   const { characters } = props;
 
-  return (
-    <WrapperPage>
-      <Styled.Title>Hello World</Styled.Title>
-      <Styled.CardList>
-        {characters.map((character) => {
-          return (
-            <CardMarvel
-              key={character.id}
-              thumbnail={`${character.thumbnail.path}/standard_amazing.${character.thumbnail.extension}`}
-              name={character.name}
-              description={character.description}
-              character={character}
-            />
-          );
-        })}
-      </Styled.CardList>
-      <ActivityIndicator />
-    </WrapperPage>
-  );
+  if (signed) {
+    return (
+      <WrapperPage>
+        <Styled.Title>Hello World, {userName}</Styled.Title>
+        <Styled.CardList>
+          {characters.map((character) => {
+            return (
+              <CardMarvel
+                key={character.id}
+                thumbnail={`${character.thumbnail.path}/standard_amazing.${character.thumbnail.extension}`}
+                name={character.name}
+                description={character.description}
+                character={character}
+              />
+            );
+          })}
+        </Styled.CardList>
+        <ActivityIndicator />
+      </WrapperPage>
+    );
+  } else {
+    return <h1>Faça Login</h1>;
+  }
 };
