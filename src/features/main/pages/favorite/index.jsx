2aeb18ec2fb8cfa8b9c0bcdced9e45e01a684860
Mateus Santos <m.santos.azeve@gmail.com>
2022-06-15 15:46:17 -0300

    mostra mensagem quando não houver favoritos

--- src/features/main/pages/favorite/index.jsx
+++ src/features/main/pages/favorite/index.jsx
@@ -5,24 +5,32 @@ import * as Styled from "./styles";
 export default (props) => {
   const { characters } = props;
 
+  console.log("characters:favorite:", characters.length);
   return (
     <WrapperPage>
       <Styled.Title>Seus Favoritos</Styled.Title>
-      <Styled.CardList>
-        {characters?.map((character) => {
-          return (
-            <CardMarvel
-              key={character.id}
-              id={character.id}
-              thumbnail={`${character.thumbnail.path}/standard_amazing.${character.thumbnail.extension}`}
-              name={character.name}
-              description={character.description}
-              character={character}
-              remove
-            />
-          );
-        })}
-      </Styled.CardList>
+
+      {characters.length !== 0 ? (
+        <Styled.CardList>
+          {characters?.map((character) => {
+            return (
+              <CardMarvel
+                key={character.id}
+                id={character.id}
+                thumbnail={`${character.thumbnail.path}/standard_amazing.${character.thumbnail.extension}`}
+                name={character.name}
+                description={character.description}
+                character={character}
+                remove
+              />
+            );
+          })}
+        </Styled.CardList>
+      ) : (
+        <Styled.Paragraph>
+          Você ainda não adicionou nenhum personagem aos favoritos
+        </Styled.Paragraph>
+      )}
     </WrapperPage>
   );
 };
--- src/features/main/pages/favorite/styles.js
+++ src/features/main/pages/favorite/styles.js
@@ -2,6 +2,10 @@ import styled from "styled-components";
 
 export const Title = styled.h1``;
 
+export const Paragraph = styled.p`
+  font-size: 20px;
+`;
+
 export const CardList = styled.div`
   display: flex;
   flex-wrap: wrap;
